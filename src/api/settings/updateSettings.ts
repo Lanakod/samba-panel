47ae1dc609d7c5a4acc65301fd0e3cba6c657b1b
Maxim Malakhov <malakhovma@lanakod.ru>
2025-07-10 16:10:28 +0300

    Fixed wrong types in settings page and UpdateSettings

--- src/api/settings/updateSettings.ts
+++ src/api/settings/updateSettings.ts
@@ -1,10 +1,21 @@
 import {UpdateSettingsResponse} from "@/interfaces"
 import {Dispatch, SetStateAction} from "react"
 
+type UpdateOk = {
+    status: true,
+    message: string,
+    setting: Record<string, string>
+}
+
+type UpdateError = {
+    status: false,
+    message: string
+}
+
 export const UpdateSettings = async (
     settings: Record<string, string>,
     setIsFetching: Dispatch<SetStateAction<boolean>>
-) => {
+): Promise<UpdateOk | UpdateError> => {
     try {
         setIsFetching(true)
         const res = await fetch('/api/server/settings', {
@@ -19,10 +30,7 @@ export const UpdateSettings = async (
         const data: UpdateSettingsResponse = await res.json()
         if (data.status) {
             setIsFetching(false)
-            return {
-                status: true,
-                setting: data.setting
-            }
+            return data
         } else {
             setIsFetching(false)
             return {
