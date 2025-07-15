2af1c17596606b4e969d67436fbe2fb3e6476532
Maxim Malakhov <malakhovma@lanakod.ru>
2025-07-15 18:50:11 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Write dynamic env file to public/env.js
-cat <<EOF > /usr/src/app/public/env.js
+cat <<EOF > /app/public/env.js
 window.__ENV__ = {
   NEXT_PUBLIC_PANEL_URL: "${NEXT_PUBLIC_PANEL_URL:-http://localhost:3000}"
 };
